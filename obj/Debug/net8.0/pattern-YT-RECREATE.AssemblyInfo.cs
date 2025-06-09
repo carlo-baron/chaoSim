195b2839f641f5729e79cbf2d73efb8d7268fc75
carlo-baron <carlobaron430@gmail.com>
2025-06-10 07:38:21 +0800

    Commit gitignore

--- obj/Debug/net8.0/pattern-YT-RECREATE.AssemblyInfo.cs
+++ obj/Debug/net8.0/pattern-YT-RECREATE.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("pattern-YT-RECREATE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+359c1c3abd7de298ec335e62f99ba7121ae6968b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54abeeca83e31a89e0a4735013a51c0cfea7af69")]
 [assembly: System.Reflection.AssemblyProductAttribute("pattern-YT-RECREATE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("pattern-YT-RECREATE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
